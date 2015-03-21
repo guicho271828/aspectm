e917e158e205d8a70e47936dbd1c2e2d32394b91
Masataro Asai <guicho2.71828@gmail.com>
2015-03-21 12:36:25 +0900

    test pass

--- src/package.lisp
+++ src/package.lisp
@@ -9,13 +9,16 @@
   (:export
    #:enable-macroexpand-hooks
    #:disable-macroexpand-hooks
+   #:macroexpand-hooks-hook
    ;; around hooks
    #:call-next-hook
-   #:add-macroexpand-hook
+   #:add-around-hook
+   #:remove-around-hook
    ;; standard hooks
-   #:define-macroexpand-hook
-   #:set-macroexpand-hook-for
-   #:remove-macroexpand-hook))
+   #:standard-hook
+   #:set-standard-hook
+   #:define-standard-hook
+   #:remove-standard-hook))
 (in-package :aspectm)
 
 ;;; enabling hooks
@@ -25,10 +28,12 @@
   (psetf *macroexpand-hook* 'macroexpand-hooks-hook
          *old-hook* *macroexpand-hook*))
 (defun disable-macroexpand-hooks ()
-  (assert (eq *macroexpand-hook* *old-hook*) nil
-          "*macroexpand-hook* is overwritten as ~a by some other program. stay alert!"
+  (assert (eq *macroexpand-hook* 'macroexpand-hooks-hook) nil
+          "*macroexpand-hook* is overwritten from ~a to ~a by some other program. stay alert!"
+          'macroexpand-hooks-hook
           *macroexpand-hook*)
-  (setf *macroexpand-hook* *old-hook*))
+  (setf *macroexpand-hook* *old-hook*)
+  (makunbound '*old-hook*))
 
 
 ;;; around-hooks
@@ -38,32 +43,34 @@
 
 ;; none of these variables should be external
 
-(let ((around-hooks '(funcall-as-hook)))
+(let (around-hooks)
   (defun add-around-hook (fname)
     (assert (symbolp fname))
     (push fname around-hooks))
   (defun remove-around-hook (fname)
-    (removef fname around-hooks)
-    (unless around-hooks
-      (warn "Popping the hooks resulted in NIL. Restoring '(funcall-as-hook)")
-      (setf around-hooks '(funcall-as-hook))))
+    (removef around-hooks fname))
   (defun macroexpand-hooks-hook (macrofn form env)
     (declare (special macrofn form env))
-    (call-next-hook))
+    (let ((around-hooks around-hooks))
+      (declare (special around-hooks))
+      (call-next-hook)))
   (defun call-next-hook ()
-    (declare (special macrofn form env))
+    (declare (special macrofn form env around-hooks))
     (restart-case
         (signal 'in-next-hook)
       (continue ()))
-    (destructuring-bind (first-hook around-hooks) around-hooks
-      (declare (special around-hooks))
-      (let (next-hook-called)
-        (handler-bind ((in-next-hook (lambda (c)
-                                       (setf next-hook-called t)
-                                       (continue c))))
-          (funcall first-hook macrofn form env))
-        (unless next-hook-called
-          (error "~a is not calling the next hook through call-next-hook!" first-hook))))))
+    (if around-hooks
+        (destructuring-bind (first-hook . around-hooks) around-hooks
+          (declare (special around-hooks))
+          (let (next-hook-called)
+            (prog1
+              (handler-bind ((in-next-hook (lambda (c)
+                                             (setf next-hook-called t)
+                                             (continue c))))
+                (funcall first-hook macrofn form env))
+              (unless next-hook-called
+                (error "~a is not calling the next hook through call-next-hook!" first-hook)))))
+        (funcall-as-hook macrofn form env))))
 
 (defun funcall-as-hook (macrofn form env)
   (restart-case
@@ -74,33 +81,38 @@
 
 ;;; standard hooks
 
-(lispn:define-namespace before-hooks (cons symbol function))
-(lispn:define-namespace after-hooks  (cons symbol function))
+(lispn:define-namespace before-hooks list)
+(lispn:define-namespace after-hooks  list)
 
 (defun standard-hook (macrofn form env)
-  `(progn
-     ,@(when-let ((hooks (ignore-errors
-                           (symbol-before-hooks
-                            (car form)))))
-         (mapcar (lambda (%) (funcall % form env)) hooks))
-     ,(call-next-hook)
-     ,@(when-let ((hooks (ignore-errors
-                           (symbol-after-hooks
-                            (car form)))))
-         (mapcar (lambda (%) (funcall % form env)) hooks))))
+  (declare (ignorable macrofn))
+  (flet ((call-hook (%) (funcall (macro-function % env) form env)))
+    (let ((befores (when-let ((hooks (ignore-errors
+                                       (symbol-before-hooks
+                                        (car form)))))
+                     (mapcar #'call-hook hooks)))
+          (main (call-next-hook))
+          (afters (when-let ((hooks (ignore-errors
+                                      (symbol-after-hooks
+                                       (car form)))))
+                    (mapcar #'call-hook hooks))))
+      (if (or (some #'identity befores)
+              (some #'identity afters))
+          `(progn ,@befores ,main ,@afters)
+          main))))
 
 (defun set-standard-hook (name hook &optional (method :before))
   (assert (member method '(:before :after)))
   (if (eq :before method)
       (progn
         (unless (before-hooks-boundp name)
-          (setf (before-hooks-boundp name) nil))
+          (setf (symbol-before-hooks name) nil))
         (pushnew hook (symbol-before-hooks name)))
       (progn
         (unless (after-hooks-boundp name)
-          (setf (after-hooks-boundp name) nil))
+          (setf (symbol-after-hooks name) nil))
         (pushnew hook (symbol-after-hooks name))))
-  tag)
+  hook)
 
 (defmacro define-standard-hook ((name &optional hook-name (method :before)) args &body body)
   (let ((hook-name (or hook-name (gensym "HOOK"))))
@@ -111,7 +123,7 @@
 (defun remove-standard-hook (name hook method)
   (assert (member method '(:before :after)))
   (if (eq :before method)
-      (removef hook (symbol-before-hooks name))
-      (removef hook (symbol-after-hooks name))))
+      (removef (symbol-before-hooks name) hook)
+      (removef (symbol-after-hooks name) hook)))
 
 
--- t/package.lisp
+++ t/package.lisp
@@ -19,8 +19,57 @@
 ;; run test with (run! test-name) 
 ;;   test as you like ...
 
-(test aspectm
+(def-fixture mhook ()
+  (enable-macroexpand-hooks)
+  (unwind-protect
+      (&body)
+    (disable-macroexpand-hooks)))
 
-  )
+(defun hellwhat (&rest args)
+  (apply #'funcall args))
+
+(test enable-disable
+  (let (tmp)
+    (setf tmp *macroexpand-hook*)
+    (unwind-protect
+        (progn
+          (finishes
+            (enable-macroexpand-hooks)
+            (setf *macroexpand-hook* 'hellwhat))
+          (signals error
+            (disable-macroexpand-hooks)))
+      (setf *macroexpand-hook* tmp)
+      (makunbound 'aspectm::*old-hook*))))
+
+
+(test (stdhook-setup :depends-on enable-disable)
+  (with-fixture mhook ()
+    (finishes
+      (add-around-hook 'standard-hook)
+      (remove-around-hook 'standard-hook))))
+
+(def-fixture std ()
+  (unwind-protect
+      (progn
+        (add-around-hook 'standard-hook)
+        (&body))
+    (remove-around-hook 'standard-hook)))
+
+
+(defvar *names* nil)
+(defmacro store-name (&whole form name args &body body)
+  (declare (ignorable args body))
+  (assert (eq (first form) 'defun))
+  (push name *names*)
+  nil)
+
+(test (stdhook :depends-on stdhook-setup)
+  (with-fixture mhook ()
+    (with-fixture std ()
+      (set-standard-hook 'defun 'store-name :before)
+      (let (*names*)
+        (finishes (eval '(defun myfunc ())))
+        (is (member 'myfunc *names*)))
+      (remove-standard-hook 'defun 'store-name :before))))
 
 
